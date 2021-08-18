0fed631c4309fdaafebc14e287ef67103c9683dd
matt <matt@company.com>
2021-08-18 19:15:32 +0100

    updated pom.xml

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("john");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
